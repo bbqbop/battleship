3f287e28782b381a157324e89871df70e6e0ea2c
bbqbop <dierk.peters@gmail.com>
2023-05-06 22:41:45 -0400

    add attack function and random computer moves

--- src/game.js
+++ src/game.js
@@ -1,56 +1,38 @@
-const { Ship } = require('./ship');
+const { GameBoard } = require('./gameboard');
 
-exports.Gameboard = function(){
-    this.board = new Array(10).fill(null).map(() => new Array(10).fill(null));
-    this.ships = {};
-    this.hasLost = false;
+exports.Game = function(){
+    this.players = {
+        player1: new GameBoard(),
+        player2: new GameBoard(),
+    };
+    this.currentPlayer = true;
+    this.compEnemy = true;
 }
-this.Gameboard.prototype = {
-    placeShip: function(length, startPos, isVert){
-        const initial = this.findShipType(length);
-        let [row, col] = startPos;
-        const maxCoordinate = isVert ? col - length + 1 : row + length - 1;
-        if (maxCoordinate < 0 || maxCoordinate > 9){
-            throw new Error('Invalid placement. Out of bounds')
-        };
-        this.ships[initial] = new Ship(length);
-        for (i = 0; i < length; i++){
-            if(isVert){
-                this.board[row][col - i] = initial;
-            }
-            else{
-                this.board[row + i][col] = initial;
-            }
-        }        
-    },
-    findShipType: function(length) {
-        switch(length){
-            case(5): return 'A';
-            case(4): return 'B';
-            case(3): return 'C';
-            case(2): return this.ships.C1 ? 'D2' : 'D1';
-            case(1): return this.ships.S1 ? 'S2' : 'S1';
+this.Game.prototype = {
+    curPlayerAttacks: function(coordinates){
+        this.currentPlayer = !this.currentPlayer
+        if(this.currentPlayer){
+            this.players.player1.receiveAttack(coordinates);
         }
-    },
-    receiveAttack: function(coordinates) {
-        const [row, col] = coordinates;
-        const field = this.board[row][col]
-        if(field != null){
-            this.ships[field].hit()
-            this.checkStatus();
+        else {
+            this.players.player2.receiveAttack(coordinates);
         }
-        this.board[row][col] = 'X';
+        if (this.compEnemy && !this.currentPlayer){
+            this.compAttack();
+        } 
     },
-    checkStatus: function(){
-        if(Object.values(this.ships).every(ship => ship.isSunk)){
-            this.hasLost = true;
-        };
+    compAttack: function(){
+        this.currentPlayer = !this.currentPlayer;
+        let row = Math.floor(Math.random() * 9);
+        let col = Math.floor(Math.random() * 9);
+        this.players.player1.receiveAttack([row,col]);
     }
-}
+};
 
-const board = new this.Gameboard;
-board.placeShip(2,[2,2],false)
-board.receiveAttack([2,2])
-board.receiveAttack([3,2])
+// const newGame = new this.Game();
+// newGame.curPlayerAttacks([2,2]);
+// console.log(newGame.players.player1.board)
+// newGame.curPlayerAttacks([4,4]);
+// console.log(newGame.players.player1.board)
 
-console.log(board)
+
--- src/game.test.js
+++ src/game.test.js
@@ -1,5 +1,6 @@
 const { Ship } = require('./ship');
-const { Gameboard } = require('./game.js');
+const { GameBoard } = require('./gameboard.js');
+const { Game } = require('./game');
 
 // SHIP FACTORY
 
@@ -23,54 +24,76 @@ test('Ship.hit() no of length times will sink it', () => {
 
 // GAMEBOARD FACTORY
 
-test('Gameboard factory initializes gameboard with 10 rows', () => {
-    const board1 = new Gameboard;
+test('GameBoard factory initializes gameboard with 10 rows', () => {
+    const board1 = new GameBoard;
     expect(board1.board.length).toBe(10);
 });
 
-test('Gameboard factory initializes gameboard with 10 colums', () => {
-    const board1 = new Gameboard;
+test('GameBoard factory initializes gameboard with 10 colums', () => {
+    const board1 = new GameBoard;
     expect(board1.board[9].length).toBe(10);
 });
 
 test('.placeShip(length, start, isVert) places ships on gameboard with ship initials', () => {
-    const board1 = new Gameboard;
+    const board1 = new GameBoard;
     board1.placeShip(3, [2, 2], false)
     expect(board1.board[2][2]).toBe('C');
     expect(board1.board[3][2]).toBe('C');
     expect(board1.board[4][2]).toBe('C');
 });
 
-test('.placeShip(length, start, isVert) stores new Ship in Gameboard.ships object', () => {
-    const board1 = new Gameboard;
+test('.placeShip(length, start, isVert) stores new Ship in GameBoard.ships object', () => {
+    const board1 = new GameBoard;
     board1.placeShip(3, [2, 2], false)
     expect(board1.ships.C).toEqual({"hitCount": 0, "isSunk": false, "length": 3});
 });
 
 test('.placeShip(length, start, isVert) throws error when its limits are out of bounds', () => {
-    const board1 = new Gameboard;
+    const board1 = new GameBoard;
     expect((() => board1.placeShip(3, [9, 2], false))).toThrow(('Invalid placement. Out of bounds'));
     ;
 });
 
 test('.receiveAttack(coordinates) stores missed attack on gameboard as X', () => {
-    const board1 = new Gameboard;
+    const board1 = new GameBoard;
     board1.receiveAttack([2,2]);
     expect(board1.board[2][2]).toBe('X');
 })
 
 test('.receiveAttack(coordinates) calls hit() on a ship when hit', () => {
-    const board1 = new Gameboard;
+    const board1 = new GameBoard;
     board1.placeShip(3, [2, 2], false);
     board1.receiveAttack([2,2]);
     expect(board1.ships.C.hitCount).toBe(1);
 })
 
 test('hasLost flips to true when all ships are sunk', () => {
-    const board1 = new Gameboard;
+    const board1 = new GameBoard;
     board1.placeShip(2, [2, 2], true);
     board1.receiveAttack([2,2]);
     board1.receiveAttack([2,1]);
     expect(board1.ships.D1.isSunk).toBe(true);
     expect(board1.hasLost).toBe(true);
+})
+
+// Game
+
+test('Game has two players when instantiated', () => {
+    const newGame = new Game();
+    expect(Object.keys(newGame.players).length).toBe(2);
+})
+
+test('Game.curPlayerAttacks has player1 attack player2, then 2 - 1', () => {
+    const newGame = new Game();
+    newGame.compEnemy = false;
+    newGame.curPlayerAttacks([2,2]);
+    newGame.curPlayerAttacks([4,4]);
+    expect(newGame.players.player2.board[2][2]).toBe('X');
+    expect(newGame.players.player1.board[4][4]).toBe('X');
+})
+
+test('Computer attacks after player 1s move', () => {
+    const newGame = new Game();
+    newGame.curPlayerAttacks([2,2]);
+    expect(newGame.players.player1.board.find(row => row.find(field => field == 'X')).length).toBe(10)
 })
