f1c7c60eaa0fa7d436f15ba4edf8b4f496c1813a
bbqbop <dierk.peters@gmail.com>
2023-05-10 14:05:15 -0400

    work on AI. it recognizes direction of 2 hits, unsure when to clear cue

--- src/game.js
+++ src/game.js
@@ -8,7 +8,7 @@ exports.Game = function(){
     this.currentPlayer = true;
     this.twoPlayer = false;
     this.gameOver = false;
-    this.compQueue = [];
+    this.compQueue = {lastHits: [], cue: []};
 }
 this.Game.prototype = {
     curPlayerAttacks: function(coords){
@@ -34,9 +34,8 @@ this.Game.prototype = {
         let col;
         while(!result){
             try{
-                if (this.compQueue.length > 0){
-                    console.log(this.compQueue);
-                    [ row, col ] = this.compQueue.shift();
+                if (this.compQueue.cue.length > 0){
+                    [ row, col ] = this.compQueue.cue.shift();
                 }
                 else {
                     row = Math.floor(Math.random() * 10);
@@ -47,11 +46,11 @@ this.Game.prototype = {
                     this.fillQueue(row, col);
                 }
                 if (result.attackResult[0] === 'SUNK!'){
-                    this.compQueue = []
+                    this.compQueue.lastHits = [];
                 }
             } catch{
-                if (this.compQueue.length > 0){
-                    [ row, col ] = this.compQueue.shift();
+                if (this.compQueue.cue.length > 0){
+                    [ row, col ] = this.compQueue.cue.shift();
                 }
                 row = Math.floor(Math.random() * 10);
                 col = Math.floor(Math.random() * 10);
@@ -60,15 +59,43 @@ this.Game.prototype = {
         return result;
     },
     fillQueue(row, col){
+        console.log('1')
+        // if hit is second hit
+            // compare with last hit
+        if (this.compQueue.lastHits.length > 0){
+            const [lastRow, lastCol] = this.compQueue.lastHits[0];
+            // if vertical
+            if (row === lastRow + 1 || row === lastRow - 1 && col === lastCol){
+                console.log(row, lastRow, 'vertical', this.compQueue.cue);
+                const fieldsInCol = [[row+1,col],[row-1,col],[lastRow+1, col],[lastRow-1, col]]
+                fieldsInCol.forEach(coord => this.compQueue.cue.unshift(coord))
+                console.log('afterwards', this.compQueue.cue)
+
+            }
+            // if horizontal
+            else {
+                console.log(col, lastCol, 'horizontal', this.compQueue.cue)
+                const fieldsInRow = [[row,col+1],[row,col-1],[row, lastCol+1],[row, lastCol-1]]
+                fieldsInRow.forEach(coord => this.compQueue.cue.unshift(coord)
+                )
+                console.log('afterwards', this.compQueue.cue)
+
+            }
+        }
+        console.log('2')
+        this.compQueue.lastHits.push([row,col]);
+        
         const surroundingCoords = [
             [row, col+1], [row+1, col],
             [row, col-1], [row-1, col]
         ]
+
         surroundingCoords.forEach(coord => {
             if(coord[0] >= 0 && coord[0] <= 9 && coord[1] >= 0 && coord[1] <= 9){
-                this.compQueue.push(coord);
+                this.compQueue.cue.push(coord);
             }
         })
+        console.log(this.compQueue)
     }
 };
 
